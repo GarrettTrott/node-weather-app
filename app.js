499dc473852870b6f7689512dd6d8ba891dcb7cf
Garrett Trott <garretttrott@gmail.com>
2020-05-19 21:50:50 -0700

    add to getWeather

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const https = require("https");
 function printError(error) {
   console.error(error.message);
 }
-//converts kelvin temps to fahrenheit
-function kelToF(kelvin) {
-  return (kelvin - 273.15) * 1.8 + 32;
+
+function isValidZip(input) {
+  return /^\d{5}$/.test(input);
+}
+
+function isValidCityState(input) {
+  return /\b[A-Z][a-zA-Z]+[ ]?[A-Z]{2}\b/.test(input);
 }
 
 function printWeather(city, temp) {
@@ -17,9 +21,48 @@ function printWeather(city, temp) {
   console.log(message);
 }
 
-function getWeather() {}
-try {
-  const request = https.get(
-    "https://api.openweathermap.org/data/2.5/weather?zip=97213&appid=0ca09cc294b445c91fbbe4f647178a23"
-  );
+function getWeather(input) {
+  let peram = "";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?${peram}${input}&units=imperial&appid=0ca09cc294b445c91fbbe4f647178a23`;
+  if (isValidZip(input)) {
+    peram = "zip=";
+  } else if (isValidCityState(input)) {
+    peram = "q=";
+  } else {
+    console.log(
+      "Please enter valid city, state or zipcode to get current weather (example: 90210) (example: Portland OR)"
+    );
+  }
+  try {
+    const request = https.get(apiURL, (response) => {
+      if (response.statusCode === 200) {
+        let body = "";
+        // read the data
+        response.on("data", (data) => {
+          body += data.toString();
+        });
+        response.on("end", () => {
+          try {
+            // parse the data
+            const report = JSON.parse(body);
+            // print the data
+            printWeather(report.sys.name, report.main.temp);
+          } catch (error) {
+            printError(error);
+          }
+        });
+      } else {
+        const message = `There was a problem getting the weather for ${input} (${
+          http.STATUS_CODES[response.statusCode]
+        })`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
+    });
+    request.on("error", (error) =>
+      console.error(`Problem with request: ${error.message}`)
+    );
+  } catch (error) {
+    printError(error);
+  }
 }
